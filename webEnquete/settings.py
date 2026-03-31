99e8cb4903171283cf93a27f4a6a3855e26e1f84
Felipe-m-s <fs33143martins@gmail.com>
2026-03-31 16:07:12 -0300

    Configurando env

--- webEnquete/settings.py
+++ webEnquete/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import os
 
 from django.contrib.messages import constants as messages
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,12 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1t4m*nmh_ut_#j-6_#2*^j+4+%d@(aql-cjq+c79!1d_*hp$n='
+# SECRET_KEY = 'django-insecure-1t4m*nmh_ut_#j-6_#2*^j+4+%d@(aql-cjq+c79!1d_*hp$n='
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = int(os.environ.get('DJANGO_DEBUG', default=0))
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = str(os.environ.get('DJANGO_ALLOWED_HOSTS')).split(',')
 
 
 # Application definition
