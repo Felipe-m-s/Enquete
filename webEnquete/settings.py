0c9180fd8ed4023e2cd9b31fc92a1ebd09766b47
Felipe-m-s <fs33143martins@gmail.com>
2026-04-08 23:49:01 -0300

    Retirando LOGIN_URL

--- webEnquete/settings.py
+++ webEnquete/settings.py
@@ -127,7 +127,6 @@ STATIC_URL = 'static/'
 STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-LOGIN_URL = 'signup'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'index'
 
